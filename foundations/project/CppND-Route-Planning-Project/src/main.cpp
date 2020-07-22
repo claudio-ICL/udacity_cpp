c24bda4f501b2f1321d0ddd3a8e8b0f2f8f10e12
Claudio Bellani <c.bellani17@imperial.ac.uk>
2020-07-22 13:06:58 +0200

    src directory of route planning project
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	new file:   project/CppND-Route-Planning-Project/src/main.cpp
    	new file:   project/CppND-Route-Planning-Project/src/model.cpp
    	new file:   project/CppND-Route-Planning-Project/src/model.h
    	new file:   project/CppND-Route-Planning-Project/src/render.cpp
    	new file:   project/CppND-Route-Planning-Project/src/render.h
    	new file:   project/CppND-Route-Planning-Project/src/route_model.cpp
    	new file:   project/CppND-Route-Planning-Project/src/route_model.h
    	new file:   project/CppND-Route-Planning-Project/src/route_planner.cpp
    	new file:   project/CppND-Route-Planning-Project/src/route_planner.h

---
+++ foundations/project/CppND-Route-Planning-Project/src/main.cpp
@@ -0,0 +1,88 @@
+#include <optional>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <io2d.h>
+#include "route_model.h"
+#include "render.h"
+#include "route_planner.h"
+
+using namespace std::experimental;
+
+static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
+{   
+    std::ifstream is{path, std::ios::binary | std::ios::ate};
+    if( !is )
+        return std::nullopt;
+    
+    auto size = is.tellg();
+    std::vector<std::byte> contents(size);    
+    
+    is.seekg(0);
+    is.read((char*)contents.data(), size);
+
+    if( contents.empty() )
+        return std::nullopt;
+    return std::move(contents);
+}
+
+int main(int argc, const char **argv)
+{    
+    std::string osm_data_file = "";
+    if( argc > 1 ) {
+        for( int i = 1; i < argc; ++i )
+            if( std::string_view{argv[i]} == "-f" && ++i < argc )
+                osm_data_file = argv[i];
+    }
+    else {
+        std::cout << "To specify a map file use the following format: " << std::endl;
+        std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;
+        osm_data_file = "../map.osm";
+    }
+    
+    std::vector<std::byte> osm_data;
+ 
+    if( osm_data.empty() && !osm_data_file.empty() ) {
+        std::cout << "Reading OpenStreetMap data from the following file: " <<  osm_data_file << std::endl;
+        auto data = ReadFile(osm_data_file);
+        if( !data )
+            std::cout << "Failed to read." << std::endl;
+        else
+            osm_data = std::move(*data);
+    }
+    // TODO 1: Get user input for start and end coordinates. 
+    float se_coord[4]; int i=0;
+    for (std::string pos : std::vector<std::string> {"start","end"})
+      for (std::string coord : std::vector<std::string> {"_x", "_y"}){
+        std::cout << "Insert "<< pos << coord << ": ";
+        std::cin >> se_coord[i];
+        if ((se_coord[i]<0.0)|(se_coord[i]>100.0)){
+          std::cout << "User must insert values between 0 and 100, but";
+          std::cout << "given " << pos << coord << " is " << se_coord[i] << std::endl;
+        } 
+        assert ((se_coord[i]>=0.0)&(se_coord[i]<=100.0));
+        i++;
+      }
+
+    // Build Model.
+    RouteModel model{osm_data};
+
+    // Create RoutePlanner object and perform A* search.
+    RoutePlanner route_planner{model, se_coord[0],se_coord[1], se_coord[2], se_coord[3]};
+    route_planner.AStarSearch();
+
+    std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
+
+    // Render results of search.
+    Render render{model};
+
+    auto display = io2d::output_surface{400, 400, io2d::format::argb32, io2d::scaling::none, io2d::refresh_style::fixed, 30};
+    display.size_change_callback([](io2d::output_surface& surface){
+        surface.dimensions(surface.display_dimensions());
+    });
+    display.draw_callback([&](io2d::output_surface& surface){
+        render.Display(surface);
+    });
+    display.begin_show();
+}
